663b6a2a2c33261d4a44163e68780c14621d543e
Joona Hoikkala <joona@kuori.org>
2020-06-26 14:35:38 +0300

    Hex encoder and decoder

---
+++ pkg/pencode/hexencoder.go
@@ -0,0 +1,15 @@
+package pencode
+
+import (
+	"encoding/hex"
+)
+
+type HexEncoder struct{}
+
+func (h HexEncoder) Encode(input []byte) ([]byte, error) {
+	return []byte(hex.EncodeToString(input)), nil
+}
+
+func (h HexEncoder) HelpText() string {
+	return "Hex string encoder"
+}
