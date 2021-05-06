2dc096160472dc32c599e7eac3d2a62afab7a439
DoI <5291556+denandz@users.noreply.github.com>
2021-05-07 04:27:24 +1200

    Added XML escaper/unescaper (#22)

---
+++ pkg/pencode/xmldecode.go
@@ -0,0 +1,24 @@
+package pencode
+
+import "encoding/xml"
+
+type XMLUnescaper struct{}
+
+type XMLInput struct {
+	Input string `xml:"input"`
+}
+
+func (u XMLUnescaper) Encode(input []byte) ([]byte, error) {
+	inputXML := `<xml><input>` + string(input) + `</input></xml>`
+
+	var out XMLInput
+	if err := xml.Unmarshal([]byte(inputXML), &out); err != nil {
+		return []byte{}, err
+	}
+
+	return []byte(out.Input), nil
+}
+
+func (u XMLUnescaper) HelpText() string {
+	return "XML unescape"
+}
---
+++ pkg/pencode/xmlencode.go
@@ -0,0 +1,23 @@
+package pencode
+
+import (
+	"bytes"
+	"encoding/xml"
+)
+
+type XMLEscaper struct{}
+
+func (u XMLEscaper) Encode(input []byte) ([]byte, error) {
+	buf := &bytes.Buffer{}
+
+	if err := xml.EscapeText(buf, input); err != nil {
+		return []byte{}, err
+	}
+
+	output := buf.Bytes()
+	return output, nil
+}
+
+func (u XMLEscaper) HelpText() string {
+	return "XML escape"
+}
