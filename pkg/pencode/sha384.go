1f38d240d1168811e4724cda0ffaa365ea9119cc
Joona Hoikkala <joohoi@users.noreply.github.com>
2021-05-13 17:25:36 +0300

    Add sha-family of hashers (#27)

---
+++ pkg/pencode/sha384.go
@@ -0,0 +1,16 @@
+package pencode
+
+import (
+	"crypto/sha512"
+	"fmt"
+)
+
+type SHA384Hasher struct{}
+
+func (m SHA384Hasher) Encode(input []byte) ([]byte, error) {
+	return []byte(fmt.Sprintf("%x", sha512.Sum384(input))), nil
+}
+
+func (m SHA384Hasher) HelpText() string {
+	return "SHA384 checksum"
+}
---
+++ pkg/pencode/sha512.go
@@ -0,0 +1,16 @@
+package pencode
+
+import (
+	"crypto/sha512"
+	"fmt"
+)
+
+type SHA512Hasher struct{}
+
+func (m SHA512Hasher) Encode(input []byte) ([]byte, error) {
+	return []byte(fmt.Sprintf("%x", sha512.Sum512(input))), nil
+}
+
+func (m SHA512Hasher) HelpText() string {
+	return "SHA512 checksum"
+}
